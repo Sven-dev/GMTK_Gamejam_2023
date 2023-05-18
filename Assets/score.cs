bf347ac0a27fb4bc25d52090a93edf00743aeb8e
Dynalope <dynalopetv@gmail.com>
2023-05-18 19:56:01 +0100

    Added Score Counter

---
+++ Assets/score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+
+{
+    public float time;
+    public Text scorelabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += 1;
+        scorelabel.text=time.ToString();
+    }
+}
