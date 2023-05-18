48ee2206845866dd4cfcdeda3662ce3f3ac3f661
Dynalope <dynalopetv@gmail.com>
2023-05-18 19:31:10 +0100

    Built The Wall

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Movement: MonoBehaviour
 {
-    public Transform character;
+    public Rigidbody2D character;
     public int movementspeed;
     public SpriteRenderer sprite;
     // Start is called before the first frame update
@@ -20,21 +20,21 @@ public class Movement: MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             
-            character.position += Vector3.right * Time.deltaTime * movementspeed;
+            character.position += Vector2.right * Time.deltaTime * movementspeed;
             sprite.flipX = false;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            character.position += Vector3.left * Time.deltaTime * movementspeed;
+            character.position += Vector2.left * Time.deltaTime * movementspeed;
             sprite.flipX = true;
 
         }
         if (Input.GetKey(KeyCode.W))
         {
-            character.position += Vector3.up * Time.deltaTime * movementspeed;
+            character.position += Vector2.up * Time.deltaTime * movementspeed;
 
         }
         if (Input.GetKey(KeyCode.S))
-            character.position += Vector3.down * Time.deltaTime * movementspeed;
+            character.position += Vector2.down * Time.deltaTime * movementspeed;
     }
 }
