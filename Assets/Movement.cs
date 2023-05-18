365ab21f645eb26bb9f1b9b9a5c6a23116f02771
Dynalope <dynalopetv@gmail.com>
2023-05-18 18:22:48 +0100

    flipped sprites

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,6 +6,7 @@ public class Movement: MonoBehaviour
 {
     public Transform character;
     public int movementspeed;
+    public SpriteRenderer sprite;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +21,12 @@ public class Movement: MonoBehaviour
         {
             
             character.position += Vector3.right * Time.deltaTime * movementspeed;
-
+            sprite.flipX = false;
         }
         if (Input.GetKey(KeyCode.A))
         {
             character.position += Vector3.left * Time.deltaTime * movementspeed;
+            sprite.flipX = true;
 
         }
         if (Input.GetKey(KeyCode.W))
