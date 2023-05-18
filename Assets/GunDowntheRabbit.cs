055806c0ebe191ecb641c1be3bb205a6607444b7
Dynalope <dynalopetv@gmail.com>
2023-05-18 18:42:18 +0100

    Added Cop AI

---
+++ Assets/GunDowntheRabbit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunDowntheRabbit : MonoBehaviour
+{
+    public Transform cop;
+    public Transform character;
+    public float copspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction= Vector3.MoveTowards(cop.position, character.position, copspeed*Time.deltaTime);
+        cop.position = direction;
+    }
+}
