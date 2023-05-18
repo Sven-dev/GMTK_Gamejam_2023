cab3e2506b3e08d6007eb10aef1f66955bd1ff92
Dynalope <dynalopetv@gmail.com>
2023-05-18 18:06:37 +0100

    Made a movement system
    its perfect

---
+++ Assets/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement: MonoBehaviour
+{
+    public Transform character;
+    public int movementspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+            character.position += Vector3.right * Time.deltaTime * movementspeed;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            character.position += Vector3.left * Time.deltaTime * movementspeed;
+
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            character.position += Vector3.up * Time.deltaTime * movementspeed;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+            character.position += Vector3.down * Time.deltaTime * movementspeed;
+    }
+}
