1de5775d903daa6f9f1ebf77dff6aeac85ab1e7f
WhySoup <svenjanssen12321@gmail.com>
2023-07-09 16:19:33 +0200

    main menu improvement

---
+++ Assets/GUI/ColorSwitcher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ColorSwitcher : MonoBehaviour
+{
+    [SerializeField] private Color ColorA;
+    [SerializeField] private Color ColorB;
+    [Space]
+    [SerializeField] private Image ImageLeft;
+    [SerializeField] private Image ImageRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(_Switch());
+    }
+
+    IEnumerator _Switch()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(8);
+
+            float progress = 0;
+            while (progress < 1)
+            {
+                progress += Mathf.Clamp01(Time.deltaTime/2);
+
+                ImageLeft.color = Color.Lerp(ColorA, ColorB, progress);
+                ImageRight.color = Color.Lerp(ColorB, ColorA, progress);
+
+
+                yield return null;
+            }
+
+            Color temp = ColorA;
+            ColorA = ColorB;
+            ColorB = temp;
+        }
+    }
+}
